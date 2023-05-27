0a28213cd7123d7f6f626e0c083971c0849528c1
Josef Lahmer (gugler gmbh) <josef.lahmer@gugler.at>
2023-05-27 15:08:51 +0200

    draft 2

--- sol2mqtt.py
+++ sol2mqtt.py
@@ -10,7 +10,7 @@ configFile = os.path.dirname(os.path.realpath(__file__)) + '/config.json'
 
 # Überprüfung ob ein Config Datei vorhanden ist sonst kommt eine Fehlermeldung und beendet das Programm
 if not os.path.exists(configFile):
-    print("Kein Configfile gefunden bitte eines anlegen.")
+    print("Kein Config.json gefunden bitte eines anlegen.")
     sys.exit(-1)
 
 # Überprüfung ob die Config gelesen werden kann
@@ -22,15 +22,15 @@ if not os.access(configFile, os.R_OK):
 config = json.load(open(configFile))
 
 # Überprüfung ob alle Daten in der Config vorhanden sind
-neededConfig = ['port', 'baudrate', 'key', 'printValue', 'useMQTT', 'mqttbrokerip', 'mqttbrokerport',
-                'mqttbrokeruser', 'mqttbrokerpasswort', 'useInfluxdb', 'influxdbip', 'influxdbport']
+neededConfig = ['mqttbrokerip', 'mqttbrokerport',
+                'mqttbrokeruser', 'mqttbrokerpasswort']
 for conf in neededConfig:
     if conf not in config:
         print(conf + ' Fehlt im Configfile!')
         sys.exit(3)
 
 
-# Schlüssel eingeben zB. "36C66639E48A8CA4D6BC8B282A793BBB"
+# solmate serial number
 sn = config['sn']
 
 # Aktulle Werte auf Console ausgeben (True | False)
@@ -56,16 +56,26 @@ mqttClient.connect(mqttBroker, mqttport, 60)
 n = sdnotify.SystemdNotifier()
 n.notify("READY=1")
 
+
+mqttid = client.serialnum
+mqttid = 0
 while True:
     print(".", end="", flush=True)
-    n.notify("WATCHDOG=1")
+    connected = False
+    while not connected:
+        try:
+            mqttClient.reconnect()
+            connected = True
+        except:
+            time.sleep(2)
     try:
         live_values = client.get_live_values()
         online = client.check_online()
         mqttClient.publish(f"eet/solmate/{client.serialnum}/live_values", json.dumps(live_values), 1)
         # mqttClient.publish(f"eet/solmate/{client.serialnum}/online", online, 1)
         for property_name in live_values.keys():
-            mqttClient.publish(f"eet/solmate/{client.serialnum}/{property_name}", live_values[property_name], 1)                
+            mqttClient.publish(f"eet/solmate/{mqttid}/{property_name}", live_values[property_name], 1)                
+        n.notify("WATCHDOG=1")
     except Exception as exc:
         print(exc)
     sleep(10)
