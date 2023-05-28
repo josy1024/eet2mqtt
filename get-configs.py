1224d9519856d52c61866850e04f02ac456b7f6f
Josef Lahmer (gugler gmbh) <josef.lahmer@gugler.at>
2023-05-28 10:40:58 +0200

    configtest

---
+++ get-configs.py
@@ -0,0 +1,39 @@
+from time import sleep
+
+from websockets.exceptions import ConnectionClosedError
+
+import solmate_sdk
+from solmate_sdk.utils import retry
+from websockets.exceptions import ConnectionClosedError
+
+client = solmate_sdk.SolMateAPIClient('S1K0506A0000xxxx')
+
+# client = solmate_sdk.SolMateAPIClient(sn)
+client.quickstart()
+def run_continuously():
+    client.quickstart()
+    while True:
+        print(f"Solmate {client.serialnum}: {client.get_live_values()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_recent_logs()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_software_version()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_grid_mode()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_user_settings()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_injection_settings()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.set_min_injection(50)}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_injection_settings()}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.set_max_injection(50)}")
+        sleep(0.1)
+        print(f"Solmate {client.serialnum}: {client.get_injection_settings()}")
+        sleep(10)
+
+
+if __name__ == "__main__":
+    run_continuously()
