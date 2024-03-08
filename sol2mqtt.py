554b36892aadb888454859c4b09f01246024b34b
LAJO, Josef Lahmer <josef.lahmer@gugler.at>
2024-03-08 20:48:52 +0100

    : fix

--- sol2mqtt.py
+++ sol2mqtt.py
@@ -198,7 +198,7 @@ while True:
                     sleeploop = 24
                 print("sol reconnect: sleep " + str(sleeploop))
                 mqttClient.publish(f"eet/solmate/{mqttid}/availability", "offline", 1, retain=True)
-                if solreconnectcounter > 10
+                if solreconnectcounter > 10:
                     n.notify("WATCHDOG=1")
                     for i in range(10):
                         sleep(sleeploop)
